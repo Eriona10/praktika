6a3f2d499b2937a31c150bede8f57f9e8d074419
rinashkodra <rs52548@ubt-uni.net>
2023-05-30 22:33:51 +0200

    Location Controller

---
+++ Location.Microservice/Controllers/LocationController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pet.Data.Entieties;
+using Pet.Interfaces;
+using Pet.ViewModels;
+
+namespace Pet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : Controller
+    {
+        private readonly PawTrackerContext _context;
+        private readonly ILocationService _locationService;
+
+
+        public LocationController(PawTrackerContext context, ILocationService locationService)
+        {
+            _context = context;
+            _locationService = locationService;
+        }
+
+        [HttpPost("add-location")]
+        public IActionResult AddLocation([FromBody] Location location)
+        {
+            _locationService.AddLocation(location);
+            return Ok("Kafsha u regjistru");
+        }
+        //public async Task<ActionResult<List<Location>>> AddLocation(Location location)
+        //{
+        //    _context.Location.Add(location);
+        //    await _context.SaveChangesAsync();
+
+        //    return Ok(await _context.Location.ToListAsync());
+        //}
+        [HttpGet]
+        public IActionResult GetAllLocation()
+        {
+            var location = _locationService.GetAll();
+            return Ok(location);
+
+        }
+
+
+        [HttpGet("get-by-id /{id}")]
+        public IActionResult GetPetById(int id)
+        {
+            var _pet = _locationService.LocationById(id);
+            return Ok(_pet);
+        }
+
+        [HttpPut("update-by-id/{Id}")]
+        public IActionResult UpdatePet(int Id, [FromBody] LocationVM location)
+        {
+            var updatedPet = _locationService.UpdateLocation(Id, location);
+            return Ok(updatedPet);
+        }
+
+
+        [HttpDelete("delete-location/{Id}")]
+        public IActionResult DeletePet(int Id)
+        {
+            _locationService.DeleteLocation(Id);
+            return Ok("Kafsha eshte fshrire");
+        }
+    }
+}
---
+++ Location.Microservice/Services/LocationService.cs
@@ -0,0 +1,72 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Pet.Data.Entieties;
+using Pet.Interfaces;
+using Pet.ViewModels;
+
+namespace Pet.Services
+{
+    public class LocationService : ILocationService
+    {
+        private readonly PawTrackerContext _context;
+
+        public LocationService(PawTrackerContext context)
+        {
+            _context = context;
+        }
+
+        public void AddLocation(Location locacion)
+        {
+            var _locacion = new Location()
+            {
+
+                Lat = locacion.Lat,
+                Long = locacion.Long,
+                DateMeasured = locacion.DateMeasured,
+                PetId= locacion.PetId
+            };
+
+            _context.Location.Add(_locacion);
+            _context.SaveChanges();
+        }
+
+        public void DeleteLocation(int Id)
+        {
+            var pet = _context.Pet.Find(Id);
+            if (pet != null)
+            {
+                _context.Pet.Remove(pet);
+                _context.SaveChanges();
+            }
+        }
+
+        public List<Location> GetAll()
+        {
+                return _context.Location.ToList();
+            
+        }
+
+        public Location LocationById(int Id)
+        {
+            return _context.Location.Find(Id);
+        }
+
+        public Location UpdateLocation(int Id, LocationVM pet)
+        {
+            var existingLocation = _context.Location.Find(Id);
+            if (existingLocation != null)
+            {
+                existingLocation.Lat = pet.Lat;
+                existingLocation.Long = pet.Long;
+                existingLocation.DateMeasured = pet.DateMeasured;
+                existingLocation.PetId =pet.PetId;
+                
+
+                _context.SaveChanges();
+            }
+
+            return existingLocation;
+        }
+    }
+}
