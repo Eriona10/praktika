6d48784e492f7404c297d4950f47a749afedeb94
rinashkodra <rs52548@ubt-uni.net>
2023-05-30 22:36:29 +0200

    Location view

---
+++ Location.Microservice/ViewModels/LocationVM.cs
@@ -0,0 +1,11 @@
+﻿namespace Pet.ViewModels
+{
+    public class LocationVM
+    {
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public DateTime DateMeasured { get; set; }
+        public int PetId { get; set; }
+       
+    }
+}
