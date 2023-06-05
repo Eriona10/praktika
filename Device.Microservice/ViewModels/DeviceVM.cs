75a54483fd66846acf07165cc631a81da2ef06c6
MalisaSadiku <94755339+MalisaSadiku@users.noreply.github.com>
2023-06-05 15:40:43 +0200

    Adding the ViewModel class too

--- Device.Microservice/ViewModels/DeviceVM.cs
+++ Device.Microservice/ViewModels/DeviceVM.cs
@@ -1,6 +1,15 @@
-﻿namespace Device.Microservice.ViewModels
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Device.Microservice.ViewModels
 {
     public class DeviceVM
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Column("uniqueId")]
+        public string UniqueId { get; set; }
     }
 }
